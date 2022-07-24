b16a40ff9f740649eb32e8d352c19e25faa4c0c3
Diwakar K B <kbdwkr@gmail.com>
2022-07-24 15:49:43 +0000

    updated

--- level1_design1/mux.v
+++ level1_design1/mux.v
@@ -11,7 +11,7 @@ module mux(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8,
             inp14, inp15, inp16, inp17, inp18, inp19, inp20,
             inp21, inp22, inp23, inp24, inp25, inp26,
             inp27, inp28, inp29, inp30;
-
+            
   output [1:0] out;
   reg [1:0] out;
 
--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,24 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-
+    
     cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.inp1.value,dut.inp3.value,dut.inp5.value,dut.inp7.value,dut.inp9.value,dut.inp11.value,dut.inp13.value,dut.inp15.value,dut.inp17.value,dut.inp19.value,dut.inp21.value,dut.inp23.value,dut.inp25.value,dut.inp27.value,dut.inp29.value=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    dut.inp0.value,dut.inp2.value,dut.inp4.value,dut.inp6.value,dut.inp8.value,dut.inp10.value,dut.inp12.value,dut.inp14.value,dut.inp16.value,dut.inp18.value,dut.inp20.value,dut.inp22.value,dut.inp24.value,dut.inp26.value,dut.inp28.value,dut.inp30.value=0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    for i in range(0,31):
+        A = i
+
+        # input driving
+        dut.sel.value = A
+
+
+        await Timer(2, units='ns')
+        dut._log.info(f'A={A:05} model={A%2:02} DUT={int(dut.out.value):02}')
+        assert dut.out.value == i%2,"failed for input select line {v},expected is {v1}".format(v=i,v1=i%2)
+    #"Adder result is incorrect: {A}  != {OUT}, expected value={EXP}".format(
+    #         A=int(dut.sel.value), OUT=int(dut.out.value), EXP=0)
